369704c28cdddbed87335071fb34815092bba571
sertacyarbas <sertacyarbas@outlook.com>
2024-09-01 22:58:37 +0300

    OrderValidatorImpl

--- src/good/invoice/impl/OrderValidatorImpl.java
+++ src/good/invoice/impl/OrderValidatorImpl.java
@@ -2,11 +2,15 @@ package good.invoice.impl;
 
 import good.InvoiceContext;
 import good.invoice.OrderValidator;
+import model.Order;
 
 public class OrderValidatorImpl implements OrderValidator {
-
     @Override
     public void validateOrders(InvoiceContext context) {
-        System.out.println("Siparişleri doğrula");
+        for (Order order : context.getOrders()) {
+            if (!order.isValid()) {
+                throw new IllegalArgumentException("Invalid order: " + order.getId());
+            }
+        }
     }
 }
